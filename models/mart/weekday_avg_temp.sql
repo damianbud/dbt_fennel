024a36fd856a3d7c61da8d166a83e3a39e339d89
damianbud <damiandbudzinski@gmail.com>
2024-04-12 16:05:18 +0200

    Weekday avg temp

---
+++ models/mart/weekday_avg_temp.sql
@@ -0,0 +1,26 @@
+WITH forecast_day_data AS (
+    SELECT 
+        (extracted_data -> 'forecast' -> 'forecastday' -> 0 ->> 'date')::DATE AS date,
+        (extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' ->> 'avgtemp_c')::NUMERIC AS avg_temp_c
+    FROM {{source("staging", "weather_raw")}}
+),
+weekday_avg_temps AS (
+    SELECT
+        EXTRACT(ISODOW FROM date) AS weekday,  -- Extracts the ISO day of the week (1 = Monday, 7 = Sunday)
+        AVG(avg_temp_c) AS average_temperature
+    FROM forecast_day_data
+    GROUP BY EXTRACT(ISODOW FROM date)
+)
+SELECT 
+    CASE 
+        WHEN weekday = 1 THEN 'Monday'
+        WHEN weekday = 2 THEN 'Tuesday'
+        WHEN weekday = 3 THEN 'Wednesday'
+        WHEN weekday = 4 THEN 'Thursday'
+        WHEN weekday = 5 THEN 'Friday'
+        WHEN weekday = 6 THEN 'Saturday'
+        WHEN weekday = 7 THEN 'Sunday'
+    END AS weekday_name,
+    average_temperature
+FROM weekday_avg_temps
+ORDER BY weekday;
